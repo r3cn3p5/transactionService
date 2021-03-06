26370c21e34c8d46b64eecce1a6323896bbbe0eb
spencer <spencer@31prime.co.uk>
2021-03-06 14:12:56 +0000

    Initial

---
+++ src/main/java/com/p31prime/transaction/TransactionApplication.java
@@ -0,0 +1,13 @@
+package com.p31prime.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p31prime/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p31prime.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
