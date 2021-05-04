6299e7be161fcb8c7d18cc907ed77101dba9aae7
spencer <spencer@31prime.co.uk>
2021-05-04 23:57:36 +0100

    Added 404 support

--- src/main/java/com/p31prime/transaction/controller/TransactionController.java
+++ src/main/java/com/p31prime/transaction/controller/TransactionController.java
@@ -2,7 +2,9 @@ package com.p31prime.transaction.controller;
 
 import com.p31prime.transaction.model.Transaction;
 import com.p31prime.transaction.service.TransactionService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.awt.print.Book;
 import java.util.Optional;
@@ -19,11 +21,17 @@ public class TransactionController implements TransactionOperations {
 
     @Override
     public Optional<Transaction> getById(Integer id) {
-        return service.getTransaction(id);
+        Optional<Transaction> transaction = service.getTransaction(id);
+
+        if (transaction.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Actor Not Found");
+
+        return transaction;
     }
 
     @Override
     public void save(Transaction transaction) {
         service.saveTransaction(transaction);
     }
+
 }
--- src/main/java/com/p31prime/transaction/database/TransactionRepository.java
+++ src/main/java/com/p31prime/transaction/database/TransactionRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TransactionRepository extends MongoRepository<Transaction, Integer> {
 
-
-
 }
